289c072938684719e2a1d53105cf3a489c3bd0d3
Alberto Gómez Díaz <agomezdiaz94@gmail.com>
2025-11-13 23:46:07 -0300

    Actualización: mejoras y corrección de errores

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    open: true
+  },
   build: {
-    outDir: "dist"
+    outDir: "dist",
+    emptyOutDir: true
   }
 });
